f7d6649ac182be9c50a65088729df26a17859125
Priya3690 <3gpriya5@gmail.com>
2019-01-29 03:31:00 +0000

    modified

--- Python/NaturalNumbers.py
+++ Python/NaturalNumbers.py
@@ -1,6 +1,6 @@
 Number = int ( input ( 'Up to which number you want to print natural numbers:' ) )
 print ('The first '+ str(Number) + ' natural numbers are')
 for Counter in range ( 0, Number, 1 ):
-  print (' ' + str ( Counter + 1 ) )
+  print (' ' + str ( Counter + 1 ) ,)
 print ( '.' )
   
