9117c24751cc9fb2822753aa566595285123afcd
Priya3690 <3gpriya5@gmail.com>
2019-01-31 06:26:13 +0000

    executed sum.c in codenvy.io

--- Cprograms/Sum.c
+++ Cprograms/Sum.c
@@ -6,6 +6,6 @@ scanf("%d", &Number1);
 printf("Enter the second number:");
 scanf("%d", &Number2);
 Sum = Number1 + Number2;
-printf("The sum of  %d and %d is %d ", Number1, Number2, Sum);
+printf("The sum of  %d and %d is %d.", Number1, Number2, Sum);
 return 0;
 }
